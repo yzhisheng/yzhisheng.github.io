94b2862a2f7fcdd774abcc5749d7478c914a1c5a
源稚生 <yzhisheng94666@gmail.com>
2025-04-17 20:43:53 +0800

    Site updated: 2025-04-17 20:43:53

--- static/countdown.js
+++ static/countdown.js
@@ -1,6 +1,6 @@
 const CountdownTimer = (() => {
     const config = {
-        targetDate: "2025-01-29",
+        targetDate: "2026-02-04",
         targetName: "春节",
         units: {
             day: { text: "今日", unit: "小时" },
