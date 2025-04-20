2e749ed6cba415ba82c14b145cfef282e926565b
源稚生 <yzhisheng94666@gmail.com>
2025-04-20 21:20:50 +0800

    Site updated: 2025-04-20 21:20:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/16/HelloWorld/"];function toRandomPost(){
+var posts=["面试题/Redis/Redis面试题集合/","uncategorized/HelloWorld/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
