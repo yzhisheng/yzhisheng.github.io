304520b3d9733eef6910c7d660f420cfd399824c
源稚生 <yzhisheng94666@gmail.com>
2025-05-25 01:02:15 +0800

    Site updated: 2025-05-25 01:02:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1fe0f6422996.html","posts/6394f1287931.html"];function toRandomPost(){
+var posts=["posts/1fe0f6422996.html","posts/6394f1287931.html","posts/f68ef47ec248.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
