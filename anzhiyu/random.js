33446a8aa3ba170d9e51d7f97ecfdbdf75c8d48a
源稚生 <yzhisheng94666@gmail.com>
2025-05-29 16:43:54 +0800

    Site updated: 2025-05-29 16:43:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1fe0f6422996.html","posts/b1dca31f7085.html","posts/fe4543a6c78f.html","posts/6394f1287931.html","posts/1fa4f10759d6.html"];function toRandomPost(){
+var posts=["posts/1fe0f6422996.html","posts/b1dca31f7085.html","posts/1fa4f10759d6.html","posts/bb0592610b44.html","posts/a703f5ddf12f.html","posts/79dc5bb335bb.html","posts/6394f1287931.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
